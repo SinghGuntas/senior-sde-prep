12a56b44b58086d634ea83ec878551b01d6c9b70
Suruchi <suru@Suruchis-MacBook-Air.local>
2024-06-12 18:24:52 +0530

    FindMissingAndRepeatedNumberInArray Question

---
+++ src/com/dsalgo/Main.java
@@ -0,0 +1,17 @@
+package com.dsalgo;
+
+import com.dsalgo.Solutions.FindRepeatedAndMissingNumber;
+import com.dsalgo.Solutions.Solution;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Solution solution = new FindRepeatedAndMissingNumber(
+                "Find the missing and repeated numbers in the array of size n"
+        );
+
+        solution.run();
+    }
+}
